71e065d3c1e22fc3af4df7b99152aa51175e6264
Dipin003 <kharayatdipin@gmail.com>
2026-02-07 17:57:29 +0530

    chore: initial Next.js setup

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='min-h-screen bg-gray-900 text-white'>
+      <h1 className='text-4xl font-bold text-center pt-20'>Welcome to My Next.js App</h1>
+    </div>
+  )
+}
+
+export default page
